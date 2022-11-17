f6d95b05cd637eabc70616de96e3121bbb7c2e49
TaoLoading <1377226380@qq.com>
2022-11-17 09:59:54 +0800

    完善hook

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,17 +1,20 @@
+/**
+ * 获取用户数据
+ */
+
 import { useQuery } from 'react-query'
 import { cleanObject } from '../utils'
 import { useHttp } from '../utils/http'
 import { User } from '../pages/projectList/component/searchPanel'
 
-const URL_PREFIX = 'users'
-
 export const useUser = (param?: Partial<User>) => {
   const client = useHttp()
   return useQuery<User[], Error>(
-    [URL_PREFIX, cleanObject(param || {})],
+    ['users', cleanObject(param || {})],
     () => {
-      return client(URL_PREFIX, {
+      return client('users', {
         data: param,
       })
-    })
+    }
+  )
 }
